8f4584003e8d9c182e46c1093df9f071b8ac3eb0
Dnyaneshwar Tupe <dbtupe3256@gmail.com>
2025-03-21 17:00:31 +0530

    Completed the project

--- youtube/downloader/views.py
+++ youtube/downloader/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from yt_dlp import YoutubeDL
+import os
 
 # Create your views here.
+
+def import_video(request):
+    if request.method == 'POST':
+        url = request.POST.get('url')
+        output_folder = 'downloads'
+
+        os.makedirs(output_folder, exist_ok=True)
+        options = {
+            "outtmpl": f"{output_folder,}/%(title)s.%(ext)s",
+            "noplaylist": False,
+        }
+
+        try:
+            with YoutubeDL(options) as ydl:
+                ydl.download([url])
+            return HttpResponse("Download completed successfully!")
+        except Exception as e:
+            return HttpResponse(f"An error occurred : {e}")
+        
+    return render(request, 'downloader/index.html')
+
+
